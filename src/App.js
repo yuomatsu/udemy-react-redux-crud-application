8285d355ea7bb3eddb8c3f2b8a85dbe9faf1b7f0
yuomatsu <07yuya08@gmail.com>
2022-08-07 15:01:41 +0900

    actionを記載した

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+const App = () => (<Counter></Counter>)
+
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
   render() {
-    return <div><h1>Hello World!</h1></div>;
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    );
   }
 }
 
